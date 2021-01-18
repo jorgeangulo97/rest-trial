c067390e825fda42aa8ca906166ef6dbd7a1b51a
Mikel-Maneru <75300050+Mikel-Maneru@users.noreply.github.com>
2021-01-18 12:08:21 +0100

    Add files via upload

--- ASP/H/app.js
+++ ASP/H/app.js
@@ -6,4 +6,19 @@ $(function () {
             $('.navbar').removeClass('active');
         }
     });
+})
+
+const header = document.querySelector('.imgFondoa');
+window.addEventListener('scroll',()=>{
+    let scrollPos=window.scrollY;
+    header.style.backgroundSize= `${scrollPos+100}%`;
+  
 });
+/*
+
+var yourImg = document.getElementById('yourImgId');
+if(yourImg && yourImg.style) {
+    yourImg.style.height = '100px';
+    yourImg.style.width = '200px';
+}
+*/
